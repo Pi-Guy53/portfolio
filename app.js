89905f796911bfaa47448b4703422ded46f33caa
UsrnGamesInc <71029215+Pi-Guy53@users.noreply.github.com>
2023-03-15 17:39:55 -0400

    fixed mouse over on the scrolling gallery

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+console.log("online");
+
 let tArr = document.getElementsByClassName("body_gallery-item-shown");
 let [...slideArr] = tArr;
 let index = 0;
@@ -32,17 +34,36 @@ function plusSlides(n) {
     tick = 0;
 }
 
+let imgArr = document.getElementsByClassName("body_gallery-item-img");
+let hovering = false;
+
+for(let i = 0; i < imgArr.length; i++)
+{
+    imgArr[i].addEventListener("mouseover", () => { hoverOver(true)});
+    imgArr[i].addEventListener("mouseout", () => {hoverOver(false)});
+}
+
+function hoverOver(val)
+{
+    hovering=val;
+}
+
 function timerReset() {
-    tick = -8;
+    tick = 0;
 }
 
 function timer() {
 
     tick += .5;
 
-    if (tick > 8) {
+    if (tick > 3) {
         tick = 0;
         plusSlides(1);
+    }
+
+    if(hovering)
+    {
+        tick = 0;
     }
 
     setTimeout(() => { timer(); }, 500);
